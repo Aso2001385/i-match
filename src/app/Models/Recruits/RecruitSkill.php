9aa0b981e51583cb79a4bdb7e2ba59166c884771
Orca_2550 <2001385@s.asojuku.ac.jp>
2022-06-22 09:33:52 +0900

    user-recruit-API作成(未テスト)

--- src/app/Models/Recruits/RecruitSkill.php
+++ src/app/Models/Recruits/RecruitSkill.php
@@ -14,4 +14,23 @@ class RecruitSkill extends Model
         'skill_id',
         'level',
     ];
+
+    public static function get_skills($recruit){
+
+        $recruit['skills'] = RecruitSkill::Join('skills', 'skills.id', '=', 'reqruit_skill.skill_id')
+        ->select('reqruit_skill.*','skills.name')
+        ->where('reqruit_skill.recruit_id',$recruit['id'])
+        ->get()->toArray();
+
+        return $recruit;
+
+    }
+
+    public static function merge_skills($recruits){
+
+        $recruits = array_map('get_skills',$recruits);
+        return $recruits;
+
+    } 
+
 }
