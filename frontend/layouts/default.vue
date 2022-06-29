c9ae156304e39909b8f16846b81b5371712b1ef3
kiyama ryota <ryo3403kiyama@icloud.com>
2022-06-30 07:58:36 +0900

    モーダル表示するページ以外を作成

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -6,19 +6,19 @@
           <v-row>
             <v-list>
               <v-list-item class="pl-1 pb-5 pa-7"
-                ><v-btn href="account" text color="link"
+                ><v-btn href="news-list" text color="link"
                   ><v-icon class="white--text ma-6"> mdi-bell</v-icon
                   ><v-span class="white--text text-h6">お知らせ</v-span></v-btn
                 ></v-list-item
               >
               <v-list-item class="pl-1 pb-5 pa-6"
-                ><v-btn href="account" text color="link"
+                ><v-btn href="bulletin-list" text color="link"
                   ><v-icon class="white--text ma-6"> mdi-bulletin-board</v-icon
                   ><v-span class="white--text text-h6">掲示板</v-span></v-btn
                 ></v-list-item
               >
               <v-list-item class="pl-1 pb-5 pa-6"
-                ><v-btn href="account" text color="link"
+                ><v-btn href="chat-list" text color="link"
                   ><v-icon class="white--text ma-6"> mdi-chat</v-icon
                   ><v-span class="white--text text-h6">チャット</v-span></v-btn
                 ></v-list-item
@@ -30,7 +30,7 @@
                 ></v-list-item
               >
               <v-list-item class="pl-1 pb-5 pa-6"
-                ><v-btn href="account" text color="link"
+                ><v-btn href="history-list" text color="link"
                   ><v-icon class="white--text ma-6"> mdi-clipboard-text-clock</v-icon
                   ><v-span class="white--text text-h6">参加履歴</v-span></v-btn
                 ></v-list-item
@@ -53,8 +53,10 @@
       </v-navigation-drawer>
     </v-flex>
     <v-app-bar color="grey darken-3" app clipped-left>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title class="white--text">i-Match</v-toolbar-title>
+      <v-app-bar-nav-icon @click="drawer = !drawer" class="white--text"></v-app-bar-nav-icon>
+      <v-toolbar-title class="white--text">
+        <v-btn href="top" text class="white--text">i-Match</v-btn>
+      </v-toolbar-title>
     </v-app-bar>
     <v-app>
       <v-main>
