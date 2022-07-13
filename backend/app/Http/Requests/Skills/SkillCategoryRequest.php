942b0d90bab969d6fa1d444afed522de95be3987
Aso2001195 <2001195@s.asojuku.ac.jp>
2022-07-13 16:06:03 +0900

    update function skill-function

--- backend/app/Http/Requests/Skills/SkillCategoryRequest.php
+++ backend/app/Http/Requests/Skills/SkillCategoryRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests\Skills;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException; 
+use Illuminate\Http\Response;
 
 class SkillCategoryRequest extends FormRequest
 {
@@ -34,5 +37,15 @@ class SkillCategoryRequest extends FormRequest
             'name.string'=>'文字列で入力してください',
             'name.max'=>'100文字以内で入力してください'
         ];
+    }
+
+    protected function failedValidation( Validator $validator )
+    {
+        $response['result'] = $validator->errors()->toArray();
+        $response['status']=Response::HTTP_UNPROCESSABLE_ENTITY;
+
+        throw new HttpResponseException(
+            response()->json($response['result'],$response['status'])
+        );
     }
 }
