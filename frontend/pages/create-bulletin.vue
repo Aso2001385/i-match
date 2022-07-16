837844e197fc15d7a03fdf6b5abed997d7cf61ac
kiyama ryota <ryo3403kiyama@icloud.com>
2022-07-16 10:57:05 +0900

    api連携/aws版

--- frontend/pages/create-bulletin.vue
+++ frontend/pages/create-bulletin.vue
@@ -332,7 +332,8 @@ export default {
       }
       console.log(sendBulletin)
       this.$axios
-        .post('http://localhost:8080/api/recruits', sendBulletin)
+        .post('http://54.65.0.194/api/recruits', sendBulletin)
+        // .post('http://localhost:8080/api/recruits', sendBulletin)
         .then(response => {
           console.log('ちゃんと通っている１')
           console.log(response.data)
