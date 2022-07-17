9719d32ce7968cb4390af6a73bf7e9cc9414f7e6
kiyama ryota <ryo3403kiyama@icloud.com>
2022-07-17 19:05:11 +0900

    APIについて

--- frontend/pages/bulletin-list.vue
+++ frontend/pages/bulletin-list.vue
@@ -7,6 +7,7 @@
       <!-- <v-col cols="2" class="mt-6">
         <v-btn href="bulletin-list" text color="link" class="white--text text-h6">メンバー募集</v-btn>
       </v-col> -->
+
       <v-col cols="12" class="mt-5 ml-10 pb-5">
         <NuxtLink to="/create-bulletin" class="white--text" style="text-decoration: none">
           <strong><v-icon color="orange">mdi-plus-box</v-icon>掲示板作成</strong>
