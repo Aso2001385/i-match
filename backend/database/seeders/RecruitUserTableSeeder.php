5a0b9d0f7f0adb90011561489c77efd5c3c8b956
Aso2001147 <2001147@s.asojuku.ac.jp>
2022-07-21 18:31:16 +0900

    チャットテストデータ

--- backend/database/seeders/RecruitUserTableSeeder.php
+++ backend/database/seeders/RecruitUserTableSeeder.php
@@ -17,8 +17,8 @@ class RecruitUserTableSeeder extends Seeder
     public function run()
     {
         //$persons_array=[2, 3, 4, 5, 3];
-        $recruit_ids=[1,1, 2,2,2, 3,3,3,3, 4,4,4,4,4, 5];
-        $user_ids=   [1,2, 2,1,3, 3,1,2,4, 4,1,2,3,5, 5];
+        $recruit_ids=[1,1, 2,2,2, 3,3,3,3, 4,4,4,4,4, 5, 6,6,6,6,6,6,6,6,6,6];
+        $user_ids=   [1,2, 2,1,3, 3,1,2,4, 4,1,2,3,5, 5, 6,1,2,3,4,5,7,8,9,10];
         Schema::disableForeignKeyConstraints();
         for($i=0; $i<count($recruit_ids);$i++){
             $recruit_user=[
